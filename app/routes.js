866e1931aa1a86650e4c4942c1faa9ae005998ce
gazjoy <post@garethjoyce.co.uk>
2020-09-21 14:04:43 +0100

    [Housekeeping] tidy up and sign post sp2

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 router.use('/sprint1/', require('./views/sprint1/_routes'))
+router.use('/sprint2/', require('./views/sprint2/_routes'))
 
 module.exports = router
