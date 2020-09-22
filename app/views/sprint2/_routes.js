4587ce1bd2d8c8f477ac64b67b27c8dcf48c6cb8
gazjoy <post@garethjoyce.co.uk>
2020-09-22 11:58:30 +0100

    [Sprint 2] initial booking screens and stubs

--- app/views/sprint2/_routes.js
+++ app/views/sprint2/_routes.js
@@ -2,8 +2,19 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res) {
-  res.redirect('sprint2');
+  res.redirect('sprint2/claimant-details');
 })
 
+router.get('/book-appointment', function (req, res) {
+  res.redirect('type');
+})
+
+router.post('/type', function (req, res) {
+  res.redirect('duration');
+})
+
+router.post('/duration', function (req, res) {
+  res.redirect('where');
+})
 
 module.exports = router
