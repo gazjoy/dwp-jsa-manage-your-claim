dcbea9f3458e7810c4dd2334f6a4668fb46bde21
gazjoy <post@garethjoyce.co.uk>
2021-01-27 20:39:48 +0000

    [Sprint 6] design diary labels

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage your new style JSA claim',
+  serviceName: 'New Style JSA future workstream',
 
   // Default port that prototype runs on
   port: '3000',
